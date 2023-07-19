d4bd0972448dc66814f5baaf7052bf4eacdcf026
RGunny <sadog94@gmail.com>
2023-07-20 00:01:54 +0900

    feat: init springboot 3.1.1

---
+++ backend/src/test/java/me/rgunny/levelup/LevelupApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rgunny.levelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
