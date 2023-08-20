bb4c98a1e762bc3d6742de86249934945da42c9e
RGunny <sadog94@gmail.com>
2023-08-21 00:15:49 +0900

    feat: AccountCreate from(Account account) 추가

--- backend/src/test/java/me/rgunny/levelup/account/AccountCreate.java
+++ backend/src/test/java/me/rgunny/levelup/account/AccountCreate.java
@@ -1,11 +1,19 @@
 package me.rgunny.levelup.account;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class AccountCreate {
 
     private String password;
     private String name;
 
+    public static AccountCreate from(Account account) {
+        return AccountCreate.builder()
+                .password(account.getPassword())
+                .name(account.getName())
+                .build();
+    }
 }
