307d6eaf130f1e3671ebb850f412caae3c63fa82
RGunny <sadog94@gmail.com>
2023-08-20 23:51:53 +0900

    feat: Account 도메인 생성

---
+++ backend/src/test/java/me/rgunny/levelup/account/Account.java
@@ -0,0 +1,22 @@
+package me.rgunny.levelup.account;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Account {
+
+    private final Long id;
+    private final String password;
+    private final String name;
+    private final long balance;
+
+    @Builder
+    public Account(Long id, String password, String name, long balance) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.balance = balance;
+    }
+
+}
