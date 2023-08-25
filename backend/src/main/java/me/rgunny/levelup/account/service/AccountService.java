bbfc87a8628f8cabd732a50fcc6cc0f251b289aa
RGunny <sadog94@gmail.com>
2023-08-25 19:09:15 +0900

    feat: add AccountService

--- backend/src/main/java/me/rgunny/levelup/account/service/AccountService.java
+++ backend/src/main/java/me/rgunny/levelup/account/service/AccountService.java
@@ -1,6 +1,31 @@
 package me.rgunny.levelup.account.service;
 
+import lombok.RequiredArgsConstructor;
+import me.rgunny.levelup.account.domain.Account;
+import me.rgunny.levelup.account.domain.AccountCreate;
+import me.rgunny.levelup.account.domain.AccountUpdate;
+import me.rgunny.levelup.account.service.port.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
 public class AccountService {
 
+    private final AccountRepository accountRepository;
 
+    public Account getById(Long id){
+        return accountRepository.findById(id).orElseThrow(() -> new NoSuchElementException("해당 ID 에 해당하는 계좌를 찾을 수 없습니다."));
+    }
+
+    public Account create(AccountCreate accountCreate){
+        return accountRepository.save(Account.from(accountCreate));
+    }
+
+    public Account update(Long id, AccountUpdate accountUpdate){
+        Account account = getById(id);
+        account = account.update(accountUpdate);
+        return accountRepository.save(account);
+    }
 }
