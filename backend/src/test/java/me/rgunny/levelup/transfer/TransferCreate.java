7bcc2b1cab1adcdade44278e185cd2f0324e269d
RGunny <sadog94@gmail.com>
2023-08-30 23:51:19 +0900

    feat: add TransferCreate

---
+++ backend/src/test/java/me/rgunny/levelup/transfer/TransferCreate.java
@@ -0,0 +1,21 @@
+package me.rgunny.levelup.transfer;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class TransferCreate {
+
+    private final Long id;
+    private final Long amount;
+    private final LocalDateTime createdDate;
+
+    public TransferCreate(Long id, Long amount, LocalDateTime createdDate) {
+        this.id = id;
+        this.amount = amount;
+        this.createdDate = createdDate;
+    }
+}
