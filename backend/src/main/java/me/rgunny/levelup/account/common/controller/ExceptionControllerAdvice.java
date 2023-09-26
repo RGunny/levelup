0b3b245a1cf74bbfc459bed11b043b2a3c14cf6f
RGunny <sadog94@gmail.com>
2023-09-26 23:40:37 +0900

    feat: add ExceptionControllerAdvice

---
+++ backend/src/main/java/me/rgunny/levelup/account/common/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,25 @@
+package me.rgunny.levelup.account.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.rgunny.levelup.account.common.domain.exception.ResourceNotFoundException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RequiredArgsConstructor
+public class ExceptionControllerAdvice {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String resourceNotFoundException(ResourceNotFoundException exception) {
+        return exception.getMessage();
+    }
+
+}
