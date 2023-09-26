60fbad2d251d4085f960e02aa75eea5cf924c22f
RGunny <sadog94@gmail.com>
2023-09-26 23:34:07 +0900

    feat: add common exception package and ResourceNotFoundException

---
+++ backend/src/main/java/me/rgunny/levelup/account/common/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package me.rgunny.levelup.account.common.domain.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String datasource, Long id) {
+        super(datasource + "에서 ID" + id + "을 찾을 수 없습니다.");
+    }
+
+}
