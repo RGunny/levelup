a05c794c8af6e8b2dde9e8fe2c12d06625d94a2f
RGunny <sadog94@gmail.com>
2023-09-29 15:11:06 +0900

    feat: add @Param on withdraw lock

--- backend/src/main/java/me/rgunny/levelup/account/infrastructure/AccountJpaRepository.java
+++ backend/src/main/java/me/rgunny/levelup/account/infrastructure/AccountJpaRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,6 +12,10 @@ public interface AccountJpaRepository extends JpaRepository<AccountEntity, Long>
 
     @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     @Query("select a from AccountEntity a where a.id=:id")
-    Optional<AccountEntity> findByIdWithPessimisticLock(Long id);
+    Optional<AccountEntity> findByIdUsingPessimisticLock(@Param("id") Long id);
+
+    @Lock(value = LockModeType.OPTIMISTIC)
+    @Query("select a from AccountEntity a where a.id=:id")
+    Optional<AccountEntity> findByIdUsingOptimisticLock(@Param("id") Long id);
 
 }
