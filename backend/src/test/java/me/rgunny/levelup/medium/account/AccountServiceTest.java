13ec898f6e26deb639c4cba744cdd857b90bdd4e
RGunny <sadog94@gmail.com>
2023-09-30 16:26:50 +0900

    config: add mysql

--- backend/src/test/java/me/rgunny/levelup/medium/account/AccountServiceTest.java
+++ backend/src/test/java/me/rgunny/levelup/medium/account/AccountServiceTest.java
@@ -1,7 +1,9 @@
 package me.rgunny.levelup.medium.account;
 
 import me.rgunny.levelup.account.domain.Account;
-import me.rgunny.levelup.account.domain.AccountCreate;
+import me.rgunny.levelup.account.facade.OptimisticLockAccountFacade;
+import me.rgunny.levelup.account.infrastructure.AccountEntity;
+import me.rgunny.levelup.account.infrastructure.AccountJpaRepository;
 import me.rgunny.levelup.account.service.AccountServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -10,9 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,32 +24,63 @@ public class AccountServiceTest {
     @Autowired
     private AccountServiceImpl accountService;
 
+    @Autowired
+    private AccountJpaRepository accountJpaRepository;
+
+    @Autowired
+    private OptimisticLockAccountFacade optimisticLockAccountFacade;
+
     @BeforeEach
     void init() {
-        AccountCreate account = AccountCreate.builder()
+        AccountEntity account = AccountEntity.builder()
                 .id(1L)
                 .number("0-1234-5678-9")
                 .password("1q2w3e4r!@")
                 .name("나라사랑계좌")
+                .balance(0L)
                 .build();
-        AccountCreate account2 = AccountCreate.builder()
+        AccountEntity account2 = AccountEntity.builder()
                 .id(2L)
                 .number("2-1234-5678-9")
                 .password("1q2w3e4r!@")
                 .name("나라사랑계좌2")
+                .balance(10000L)
+                .build();
+        AccountEntity account3 = AccountEntity.builder()
+                .id(3L)
+                .number("3-1234-5678-9")
+                .password("1q2w3e4r!@")
+                .name("나라사랑계좌3")
+                .balance(50000L)
                 .build();
 
-        accountService.create(account);
-        accountService.create(account2);
-
-        accountService.deposit(2L, 10000L);
+        accountJpaRepository.save(account);
+        accountJpaRepository.save(account2);
+        accountJpaRepository.save(account3);
     }
 
-    @DisplayName("동시에 100번 출금할 경우 pessimistic lock을 사용한 동시성 제어를 위한 테스트")
-    @Test
-    void withdrawUsingPessimisticLockConcurrentTest() throws InterruptedException {
-        Account account1 = accountService.getById(2L);
+//    @AfterEach
+//    public void delete() {
+//        accountJpaRepository.deleteAll();
+//    }
 
+    @DisplayName("amount를 계좌에서 출금하면 기존잔고에서 amount만큼 잔고가 차감된다.")
+    @Test
+//    @Rollback(value = false)
+    void withdrawSuccessTest(){
+        // given
+        long amount = 10000L;
+
+        // when
+        Account account = accountService.withdraw(2L, amount);
+
+        // then
+        assertThat(account.getBalance()).isEqualTo(0L);
+    }
+
+    @DisplayName("동시에 100번 출금할 경우 synchronized를 사용한 동시성 제어 테스트")
+    @Test
+    void withdrawUsingSynchronizedConcurrentTest() throws InterruptedException {
         // given
         int threadCount = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
@@ -57,11 +88,9 @@ public class AccountServiceTest {
 
         // when
         for (int i = 0; i < threadCount; i++) {
-            int finalI = i;
             executorService.submit(() -> {
                 try {
-                    Account account = accountService.withdrawUsingPessimisticLock(2L, 100L);
-                    System.out.println(finalI + "i : " + account.getBalance());
+                    accountService.withdrawUsingSynchronized(2L, 100L);
                 } finally {
                     countDownLatch.countDown();
                 }
@@ -72,6 +101,42 @@ public class AccountServiceTest {
         Account account = accountService.getById(2L);
 
         // then
+        assertThat(account.getBalance()).isEqualTo(0L);
+    }
+
+    @DisplayName("동시에 100번 출금할 경우 pessimistic lock을 사용한 동시성 제어를 위한 성공 테스트")
+    @Test
+    void withdrawUsingPessimisticLockConcurrentTest() throws InterruptedException {
+        // given
+        int threadCount = 100, successCount = 0;
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+                accountService.withdrawUsingPessimisticLock(2L, 100L);
+                return "Thread: " + Thread.currentThread().getName();
+            }, executorService).exceptionally(e -> {
+                System.out.println("e.getMessage() = " + e.getMessage());
+                throw new RuntimeException("RuntimeException!");
+            });
+
+            try {
+                completableFuture.join();
+                successCount++;
+            } catch (CompletionException e) {
+                throw e;
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+
+        countDownLatch.await();
+        Account account = accountService.getById(2L);
+
+        // then
+        assertThat(successCount).isEqualTo(100);
         assertThat(account.getBalance()).isEqualTo(0L);
     }
 
