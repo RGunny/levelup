9e1b5f6eb5dede724b2d142468d54d93c5eab4b9
RGunny <sadog94@gmail.com>
2023-10-11 21:52:56 +0900

    feat: add AccountController getById()

---
+++ backend/src/main/java/me/rgunny/levelup/account/controller/AccountController.java
@@ -0,0 +1,33 @@
+package me.rgunny.levelup.account.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.rgunny.levelup.account.controller.port.AccountService;
+import me.rgunny.levelup.account.controller.response.AccountResponse;
+import me.rgunny.levelup.account.domain.Account;
+import me.rgunny.levelup.account.domain.AccountCreate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> create(@RequestBody AccountCreate accountCreate) {
+        Account account = accountService.create(accountCreate);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(AccountResponse.from(account));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok()
+                .body(AccountResponse.from(accountService.getById(id)));
+    }
+
+}
